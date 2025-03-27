ed8714423a86422a0bd88965e50f346252c9a0dd
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2025-03-27 23:21:25 +0700

    Fix: add modifications to the register page

--- xplorium/src/app/register/page.tsx
+++ xplorium/src/app/register/page.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import SignUpPage from "@/components/SignUpPage"; // Ensure path is correct
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+    // Checks if the user has session
+    const session = await auth()
+
+    // If user has session redirect them to chatroom
+    if (session) {
+        redirect("/chatroom")
+    }
+
     return <SignUpPage />;
 }
