0a399c4bdeed98d6c2d98099d5949a1ae33ddbf2
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2025-03-27 23:21:02 +0700

    Fix: add modifications to the login page

--- xplorium/src/app/login/page.tsx
+++ xplorium/src/app/login/page.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import LoginPage from "@/components/LoginPage"; // Ensure path is correct
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+    // Checks if the user has seesion
+    const session = await auth();
+
+    // If user has session redirect them to chatroom
+    if (session) {
+        redirect("/chatroom");
+    }
+
     return <LoginPage />;
 }
