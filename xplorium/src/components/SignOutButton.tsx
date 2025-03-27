32c47edfe424d04a2e3127b701efee6dea0d7f7f
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2025-03-27 23:23:48 +0700

    Feat: add sign out button

---
+++ xplorium/src/components/SignOutButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+
+export default function SignOutButton() {
+  return (
+    <button
+      onClick={() => signOut({redirectTo: "/"})}
+      className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition" 
+    >
+      Sign Out
+    </button>
+  );
+}
